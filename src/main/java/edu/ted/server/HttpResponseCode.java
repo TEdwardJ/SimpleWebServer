7a7bfe7d9b6083e590fcc3dde2329dbe4007c480
TedwardJ <edt77@ukr.net>
2020-06-22 20:32:07 +0300

    Changes in HttpResponseCode Class:  - field name should not start with capital letter (silly me);  - class has been transformed to enum, since enums "spirit" responds the purpose of the class

--- src/main/java/edu/ted/server/HttpResponseCode.java
+++ src/main/java/edu/ted/server/HttpResponseCode.java
@@ -1,18 +1,18 @@
 package edu.ted.server;
 
-public final class HttpResponseCode {
+public enum HttpResponseCode {
 
-    public static final HttpResponseCode OK = new HttpResponseCode(200, "OK");
-    public static final HttpResponseCode NOT_FOUND = new HttpResponseCode(404, "Not Found");
-    public static final HttpResponseCode METHOD_NOT_ALLOWED = new HttpResponseCode(405, "Method Not Allowed");
-    public static final HttpResponseCode INTERNAL_ERROR = new HttpResponseCode(500, "Internal Server Error");
+    OK(200, "OK"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_ERROR(500, "Internal Server Error");
 
-    private int code;
-    private String ReasonPhrase;
+    private final int code;
+    private String reasonPhrase;
 
     private HttpResponseCode(int code, String reasonPhrase) {
         this.code = code;
-        ReasonPhrase = reasonPhrase;
+        this.reasonPhrase = reasonPhrase;
     }
 
     public int getCode() {
@@ -20,6 +20,6 @@ public final class HttpResponseCode {
     }
 
     public String getReasonPhrase() {
-        return ReasonPhrase;
+        return reasonPhrase;
     }
 }
